304c4ef2a1d967507112a8b80f584e8500d51464
JeremyRamirezG <jrg7148@gmail.com>
2022-11-20 15:17:59 -0600

    Clases con Atributos

--- ProyectoFinal_SistemaDeVentaDeVerduleria/src/main/java/sc202/proyectofinal_sistemadeventadeverduleria/Frutas.java
+++ ProyectoFinal_SistemaDeVentaDeVerduleria/src/main/java/sc202/proyectofinal_sistemadeventadeverduleria/Frutas.java
@@ -1,5 +1,26 @@
 package sc202.proyectofinal_sistemadeventadeverduleria;
 
 public class Frutas extends Productos {
+    //Atributos propios de la clase
+    private String temporadaFrutas;
+    
+    //Constructores
+    public Frutas(String temporadaFrutas, String nombreProducto, Double precioProducto, String fechaIngreso, int cantidadProducto) {
+        super(nombreProducto, precioProducto, fechaIngreso, cantidadProducto);
+        this.temporadaFrutas = temporadaFrutas;
+    }
+    public Frutas() {
+        super();
+        this.temporadaFrutas = "";
+    }
+    
+    //GETs y SETs
+    public String getTemporadaFrutas() {
+        return temporadaFrutas;
+    }
+
+    public void setTemporadaFrutas(String temporadaFrutas) {
+        this.temporadaFrutas = temporadaFrutas;
+    }
     
 }
--- ProyectoFinal_SistemaDeVentaDeVerduleria/src/main/java/sc202/proyectofinal_sistemadeventadeverduleria/Verduras.java
+++ ProyectoFinal_SistemaDeVentaDeVerduleria/src/main/java/sc202/proyectofinal_sistemadeventadeverduleria/Verduras.java
@@ -1,5 +1,26 @@
 package sc202.proyectofinal_sistemadeventadeverduleria;
 
 public class Verduras extends Productos {
+    //Atributos propios de la clase
+    private String temporadaVerduras;
+    
+    //Constructores
+    public Verduras(String temporadaVerduras, String nombreProducto, Double precioProducto, String fechaIngreso, int cantidadProducto) {
+        super(nombreProducto, precioProducto, fechaIngreso, cantidadProducto);
+        this.temporadaVerduras = temporadaVerduras;
+    }
+    public Verduras() {
+        super();
+        this.temporadaVerduras = "";
+    }
+    
+    //GETs y SETs
+    public String getTemporadaVerduras() {
+        return temporadaVerduras;
+    }
+
+    public void setTemporadaVerduras(String temporadaVerduras) {
+        this.temporadaVerduras = temporadaVerduras;
+    }
     
 }
